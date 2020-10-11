f4b659bb16d34d2b581d924ed7b0c7731b127164
Rushil Baya <rushil.baya@gmail.com>
2020-10-11 11:06:48 +0800

    added tests

--- methods.py
+++ methods.py
@@ -10,22 +10,24 @@ class Vec2:
         if type(v) == Vec2:
             return Vec2(self.x + v.x, self.y + v.y)
         else:
-            raise TypeError
+            raise TypeError('Argument must be a Vec2 object.')
 
     def sub(self, v):
         if type(v) == Vec2:
             return Vec2(self.x - v.x, self.y - v.y)
         else:
-            raise TypeError
+            raise TypeError('Argument must be a Vec2 object.')
 
     def mult(self, n):
         if type(n) == int or type(n) == float:
             return (self.x * n, self.y * n)
         else:
-            raise TypeError
+            raise TypeError('Argument must be an int or a float.')
 
 x = Vec2(3, 5)
 y = Vec2(2, 9)
+z = 4
 print(x.sub(y))
-print(y.mult(3))
+print(y.mult(z))
 print(y.add(x))
+print(x.add(z))
